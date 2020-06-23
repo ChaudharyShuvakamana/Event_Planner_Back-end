7ed16254fcbc9df06da05d8ff728b03b2627302f
sameeeeer <lastking.me01@gmail.com>
2020-06-23 13:34:58 +0545

    User login created

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,52 @@ let jwt = require('jsonwebtoken');
 let config = require('../config');
 const bcrypt = require('bcrypt');
 
-//function for adding trip
+exports.login =(req, res) => {
+    user.findOne({email : req.body.email}, function(err, user){
+      if(err){
+          return res.status(500).send('Error on the server');
+          
+      }
+      
+      
+      if(!user){
+          return res.status(401).json({
+              success : false,
+              message: 'Incorrent username or password',
+              token : null
+          });
+      }
+      
+      var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+      if(!passwordIsValid){
+          return res.status(401).json({
+              success : false,
+              message: 'Incorrent username or password',
+              token : null
+          });
+      }
+
+    
+      let token = jwt.sign({id : user._id.toString()},config.secret, 
+          {
+              expiresIn : '24h'
+          }) ;
+          user.tokens = user.tokens.concat({ token: token });
+           user.save();
+  
+         return res.status(200).json({
+              success : true,
+              message : 'Authentication successful',
+              token : token,
+              name : user.firstname + " " + user.lastname,
+              email : user.email,
+              image : user.profileImg,
+              userid : user._id
+          });
+    });
+  }
+
+//function for adding user
 exports.adduser =(req, res) => {
         var hashedPassword = bcrypt.hashSync(req.body.password, 8);
         
@@ -39,4 +84,29 @@ exports.adduser =(req, res) => {
         
         );
         
+        }
+        
+        exports.checkEmailAvailabilityser =(req, res) => {
+            user.findOne({email : req.body.email}, function(err, user){
+                if(err){
+                    return res.status(500).send('Error on the server');
+                    
+                }
+        
+                if(user){
+                   
+                    return res.send({
+                        success : false,
+                        message: 'Email Already Exists',
+                        
+                    });
+                }else{
+                    return res.send({
+                        success : true,
+                        message: 'Email Available',
+                        
+                    });
+                }
+                
+            })
         }
--- router/userRouter.js
+++ router/userRouter.js
@@ -4,7 +4,9 @@ const Users = require('../models/User')
 const auth = require('../middleware/auth');
 const UserController = require('../controller/userController');
 
+
 router.post("/register",UserController.adduser)
+router.post('/checkemail', UserController.checkEmailAvailabilityser)
 
 
 
