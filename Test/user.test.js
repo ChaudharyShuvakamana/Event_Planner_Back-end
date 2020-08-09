8efe1b70904a2bf77026dc035f14a2c4a6001808
Narayan5876 <naran5876@gmail.com>
2020-08-09 13:32:31 +0545

    userupdate

--- Test/user.test.js
+++ Test/user.test.js
@@ -27,6 +27,23 @@
 //             });
 //     });
 // });
+
+it('to test the update', async () => {
+
+    return user.findOneAndUpdate({_id :Object('5f06cab9eb2aaa22a4c481e7')}, {$set : {firstname:'sameer',lastname:'karki',location:'Nepal',
+    phonenumber:'987456321', email:'sameer@gmail.com', password: 'sam00sau35'}})
+    .then((pp)=>{
+        expect(pp.firstname).toEqual('xyz'),
+        expect(pp.lastname).toEqual('a.jpg'),
+        expect(pp.location).toEqual('xyz'),
+        expect(pp.phonenumber).toEqual('9849635014'),
+        expect(pp.email).toEqual('naran@gmail.com'),
+        expect(pp.password).toEqual('naran12')
+       
+        
+
+    })
+});
 // // it('Testing of User Deletion', async() => {
 // //     const status = await User.deleteOne({ "_id": "5e44f4249cf67443d4fdb28c" });
 // //     expect(status.ok).toBe(1);
