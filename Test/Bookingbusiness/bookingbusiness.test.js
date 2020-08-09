09b31e3d8b4640a98ee46ecadd3086cdc8f8e5f6
Narayan5876 <naran5876@gmail.com>
2020-08-09 14:33:06 +0545

    gitdeletebookingbussines

--- Test/Bookingbusiness/bookingbusiness.test.js
+++ Test/Bookingbusiness/bookingbusiness.test.js
@@ -1,5 +1,6 @@
 var BookingBusiness = require('../../models/bookingbusiness')
 const mongoose = require('mongoose');
+const bookingbusiness = require('../../models/bookingbusiness');
 const url = 'mongodb://127.0.0.1:27017/blog';
 beforeAll(async() => {
     await mongoose.connect(url, {
@@ -10,26 +11,50 @@ beforeAll(async() => {
 afterAll(async() => {
     await mongoose.connection.close();
 });
-describe(' Testing of bookingbusiness Schema', () => {
-    it(' Testing of Adding bookingbusiness', () => {
-        const bookingbusiness = {
-            'user_id': '5e44def6d69dbe0860bddfa3',
-            'fullname': 'abc',
-            'address': 'ktm',
-            'number': '9849635014',
-            'email': 'naran@gmail.com',
-            'eventname': 'marriage',
-            'from': '1staug',
-            'to':'3rdaug',
-            'business_id': '5e44def6d69dbe0860bddfa3'
-        };
-        return BookingBusiness.create(bookingbusiness)
-        .then((bookingbusiness) => {
-            expect(bookingbusiness.email).toEqual('naran@gmail.com');
-        });
-});
+// describe(' Testing of bookingbusiness Schema', () => {
+//     it(' Testing of Adding bookingbusiness', () => {
+//         const bookingbusiness = {
+//             'user_id': '5e44def6d69dbe0860bddfa3',
+//             'fullname': 'abc',
+//             'address': 'ktm',
+//             'number': '9849635014',
+//             'email': 'naran@gmail.com',
+//             'eventname': 'marriage',
+//             'from': '1staug',
+//             'to':'3rdaug',
+//             'business_id': '5e44def6d69dbe0860bddfa3'
+//         };
+//         return BookingBusiness.create(bookingbusiness)
+//         .then((bookingbusiness) => {
+//             expect(bookingbusiness.email).toEqual('naran@gmail.com');
+//         });
+// });
 
        
+//     });
+
+    // it('to test the update bookingbusiness', async () => {
+
+    //     return bookingbusiness.findByIdAndUpdate({_id :Object('5f2fb33001ad15516476bbe0')}, {$set : {user_id:'5e44def6d69dbe0860bddfa3',fullname:'abc',address:'ktm',
+    //     number:'9849635014', email:'naran@gmail.com', eventname: 'marriage', from: '1staug', to: '3rdaug', business_id: '5e44def6d69dbe0860bddfa3'}})
+    //     .then((pp)=>{
+    //         expect(pp.user_id).toEqual('5e44def6d69dbe0860bddfa3'),
+    //         expect(pp.fullname).toEqual('naran'),
+    //         expect(pp.address).toEqual('xyz'),
+    //         expect(pp.number).toEqual('9849635014'),
+    //         expect(pp.email).toEqual('naran@gmail.com'),
+    //         expect(pp.eventname).toEqual('bday'),
+    //         expect(pp.from).toEqual('1staug'),
+    //         expect(pp.to).toEqual('2ndaug'),
+    //         expect(pp.business_id).toEqual('5e44def6d69dbe0860bddfa3')
+            
+    
+    //     })
+    // });
+
+    it('Testing of User Deletion', async() => {
+        const status = await venue.deleteOne({ "_id": "5f2fb33001ad15516476bbe0" });
+        expect(status.ok).toBe(1);
     });
 
 
